0b6518aeb5f581967573533f4f892af10e15b6d7
Kokishodome <159949398+Kokishodome@users.noreply.github.com>
2025-01-11 09:52:28 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+let boxRandomNumber=Math.random()
+console.log(boxRandomNumber);
+// Object
 let story={
+
     frontYard:{
         text:'Objective: Enter the house <br> Place: Front Yard ',
+        // array
         choices:[
             {text:'Check the box', place:'box'},
             {text:'Check the shed', place:'shed'},
             {text:'Guess the password of the door', place:'door'},
-            {text:'Stay still', place:'frontYard'},
+            {text:'Stay still', place:'stayStill'},
             {text:'Escape the Front Yard', place:'street'},
 
         ]
@@ -19,6 +24,53 @@ let story={
 
         ]
     },
+
+    note:{
+
+        text:boxRandomNumber>0.5?'password is skibidi Rizzler ':'check the shed',
+        choices:[
+            
+            {text:'Go back', place:'frontYard'},
+            
+
+        ]
+    },
+
+    shed:{
+        text:boxRandomNumber<0.5?'You find the light switch for the house. password is skibidi Rizzler ':'You find the light switch for the house',
+        choices:[
+            // {text:'Turn it on', place:'note'},
+            {text:'Go back', place:'frontYard'},
+            
+
+        ]
+    },
+
+    door:{
+        text:'Try to guess the code of the door ',
+        choices:[
+            // {text:'Turn it on', place:'note'},
+            {text:'Try to guess', place:'frontYard'},
+            {text:'Shake it rapidly', place:'frontYard'},
+            {text:'Go back', place:'frontYard'},
+            
+
+        ]
+    },
+
+    stayStill:{
+        text:'just stay there ',
+        choices:[
+            {text:'Go back', place:'frontYard'},
+        ]
+    },
+    street:{
+        text:'You get hit by a car ',
+        choices:[ 
+
+        ]
+    },
+    
 }
 let text=document.getElementById('text')
 let buttons=document.getElementById('buttons')
@@ -34,10 +86,12 @@ function showPlace(place){
         button.innerHTML=story[place].choices[i].text
         buttons.appendChild(button)
         button.onclick=function(){
-            showPlace('box')
+            showPlace(story[place].choices[i].place)
         }
     } 
 }
 
 
 startGame()
+
+// continue story line and make pictures
